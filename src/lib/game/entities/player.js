2ac6474bd34aeb160163ca2299e7ba94c04e1622
Bas Peschier <basp@fabrique.nl>
2012-11-23 16:00:29 +0100

    Point and click!

--- src/lib/game/entities/player.js
+++ src/lib/game/entities/player.js
@@ -1,148 +1,90 @@
 ig.module(
-	'game.entities.player'
+    'game.entities.player'
 )
-.requires(
-	'impact.entity'
+    .requires(
+    'impact.entity'
 )
-.defines(function(){
+    .defines(function () {
 
-EntityPlayer = ig.Entity.extend({
-	
-	// The players (collision) size is a bit smaller than the animation
-	// frames, so we have to move the collision box a bit (offset)
-	size: {x: 50, y:84},
-	offset: {x: 5, y: 0},
-	
-	maxVel: {x: 200, y: 200},
-	friction: {x: 600, y: 0},
-	
-	type: ig.Entity.TYPE.A, // Player friendly group
-	checkAgainst: ig.Entity.TYPE.NONE,
-	collides: ig.Entity.COLLIDES.PASSIVE,
-	
-	animSheet: new ig.AnimationSheet( 'media/character1.png', 60, 84 ),	
-	
-	
-	// These are our own properties. They are not defined in the base
-	// ig.Entity class. We just use them internally for the Player
-	flip: false,
-	accelGround: 400,
-	accelAir: 200,
-	jump: 200,
-	health: 10,
-	flip: false,
-	
-	init: function( x, y, settings ) {
-		this.parent( x, y, settings );
-		
-		// Add the animations
-		this.addAnim( 'idle', .5, [0, 1] );
-		this.addAnim( 'run', 0.07, [0, 1] );
-		this.addAnim( 'jump', 1, [0] );
-		this.addAnim( 'fall', 0.4, [0] );
-	},
-	
-	
-	update: function() {
-		
-		// move left or right
-		var accel = this.standing ? this.accelGround : this.accelAir;
-		if( ig.input.state('left') ) {
-			this.accel.x = -accel;
-			this.flip = true;
-		}
-		else if( ig.input.state('right') ) {
-			this.accel.x = accel;
-			this.flip = false;
-		}
-		else {
-			this.accel.x = 0;
-		}
-		
-		
-		// jump
-		if( this.standing && ig.input.pressed('jump') ) {
-			this.vel.y = -this.jump;
-		}
-		
-		// shoot
-		if( ig.input.pressed('shoot') ) {
-			ig.game.spawnEntity( EntitySlimeGrenade, this.pos.x, this.pos.y, {flip:this.flip} );
-		}
-		
-		// set the current animation, based on the player's speed
-		if( this.vel.y < 0 ) {
-			this.currentAnim = this.anims.jump;
-		}
-		else if( this.vel.y > 0 ) {
-			this.currentAnim = this.anims.fall;
-		}
-		else if( this.vel.x != 0 ) {
-			this.currentAnim = this.anims.run;
-		}
-		else {
-			this.currentAnim = this.anims.idle;
-		}
-		
-		this.currentAnim.flip.x = this.flip;
-		
-		
-		// move!
-		this.parent();
-	}
-});
+        EntityPlayer = ig.Entity.extend({
+
+            // The players (collision) size is a bit smaller than the animation
+            // frames, so we have to move the collision box a bit (offset)
+            size:{x:50, y:84},
+            offset:{x:5, y:0},
+
+            maxVel:{x:200, y:200},
+            friction:{x:600, y:0},
+
+            type:ig.Entity.TYPE.A, // Player friendly group
+            checkAgainst:ig.Entity.TYPE.NONE,
+            collides:ig.Entity.COLLIDES.PASSIVE,
+
+            animSheet:new ig.AnimationSheet('media/character1.png', 60, 84),
 
 
-// The grenades a player can throw are NOT in a separate file, because
-// we don't need to be able to place them in Weltmeister. They are just used
-// here in the code.
+            // These are our own properties. They are not defined in the base
+            // ig.Entity class. We just use them internally for the Player
+            accelGround:400,
+            accelAir:200,
+            jump:200,
+            health:10,
+            flip:false,
 
-// Only entities that should be usable in Weltmeister need to be in their own
-// file.
-EntitySlimeGrenade = ig.Entity.extend({
-	size: {x: 4, y: 4},
-	offset: {x: 2, y: 2},
-	maxVel: {x: 200, y: 200},
-	
-	
-	// The fraction of force with which this entity bounces back in collisions
-	bounciness: 0.6, 
-	
-	type: ig.Entity.TYPE.NONE,
-	checkAgainst: ig.Entity.TYPE.B, // Check Against B - our evil enemy group
-	collides: ig.Entity.COLLIDES.PASSIVE,
-		
-	animSheet: new ig.AnimationSheet( 'media/slime-grenade.png', 8, 8 ),
-	
-	bounceCounter: 0,
-	
-	
-	init: function( x, y, settings ) {
-		this.parent( x, y, settings );
-		
-		this.vel.x = (settings.flip ? -this.maxVel.x : this.maxVel.x);
-		this.vel.y = -50;
-		this.addAnim( 'idle', 0.2, [0,1] );
-	},
-		
-	handleMovementTrace: function( res ) {
-		this.parent( res );
-		if( res.collision.x || res.collision.y ) {
-			
-			// only bounce 3 times
-			this.bounceCounter++;
-			if( this.bounceCounter > 3 ) {
-				this.kill();
-			}
-		}
-	},
-	
-	// This function is called when this entity overlaps anonther entity of the
-	// checkAgainst group. I.e. for this entity, all entities in the B group.
-	check: function( other ) {
-		other.receiveDamage( 10, this );
-		this.kill();
-	}	
-});
+            init:function (x, y, settings) {
+                this.parent(x, y, settings);
+                this.dest = {x:x, y:y};
+                this.queue = [];
 
-});
+                // Add the animations
+                this.addAnim('idle', .5, [0, 1]);
+                this.addAnim('run', 0.07, [0, 1]);
+                this.addAnim('jump', 1, [0]);
+                this.addAnim('fall', 0.4, [0]);
+            },
+
+
+            update:function () {
+
+                // move left or right
+                var accel = this.standing ? this.accelGround : this.accelAir;
+                if (ig.input.pressed('click')) {
+                    this.dest = {
+                        x:ig.game.screen.x + ig.input.mouse.x - this.size.x/2,
+                        y:ig.game.screen.y + ig.input.mouse.y
+                    };
+                }
+
+                var dist = (this.vel.x*this.vel.x)/(2*this.friction.x);
+                var real_dist = Math.abs(this.pos.x - this.dest.x);
+                if (real_dist <= dist) {
+                    this.accel.x = 0;
+                } else if (this.pos.x > this.dest.x && real_dist > 1) {
+                    this.flip = true;
+                    this.accel.x = -accel;
+                } else if (this.pos.x < this.dest.x && real_dist > 1) {
+                    this.flip = false;
+                    this.accel.x = accel;
+                }
+
+                if (this.vel.x == 0 && this.accel.x == 0) {
+                    this.currentAnim = this.anims.idle;
+                    this.dest = this.pos;
+                } else {
+                    this.currentAnim = this.anims.run;
+                }
+
+
+                this.currentAnim.flip.x = this.flip;
+
+                // move!
+                this.parent();
+            }
+        });
+
+        TargetEntity = ig.Entity.extend({
+
+        })
+
+
+    });
