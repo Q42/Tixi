3c0c49a198db428eea0876eb1a55e4c342c0bf9f
Guus Goossens <guus.goossens@gmail.com>
2012-11-23 20:11:49 +0100

    Ambilight jonguh! :D

--- src/lib/game/main.js
+++ src/lib/game/main.js
@@ -1,23 +1,49 @@
-ig.module( 
-	'game.main' 
+ig.module(
+	'game.main'
 )
 .requires(
 	'impact.game',
 	'impact.font',
-	
+
 	'game.entities.player',
 	'game.levels.1'
 )
 .defines(function(){
 
+
+
+// Helper shizzle voor ambilight.
+var _lastUpdated = new Date()
+
+function _rgbToHex(r, g, b) {
+	r = Math.round( r );
+	g = Math.round( g );
+	b = Math.round( b );
+	return ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+function _hexForTimestamp( timestamp ) {
+	var frequency = 0.3
+	  , duration = 120*1000.0
+	  , offset = 32.0 * (timestamp % duration) / duration
+	  , red = Math.sin(frequency * offset + 0) * 127 + 128
+	  , green = Math.sin(frequency * offset + 2) * 127 + 128
+	  , blue  = Math.sin(frequency * offset + 4) * 127 + 128
+	  , hex = _rgbToHex(red, green, blue)
+
+	return hex;
+}
+
+
+
 MyGame = ig.Game.extend({
-	
+
 	gravity: 300, // All entities are affected by this
-	
+
 	// Load a font
 	font: new ig.Font( 'media/04b03.font.png' ),
-	
-	
+
+
 	init: function() {
 		// Bind keys
 		ig.input.bind( ig.KEY.LEFT_ARROW, 'left' );
@@ -27,12 +53,12 @@ MyGame = ig.Game.extend({
 		ig.music.add( 'media/DST-ByTheField.mp3' );
 		ig.music.volume = 1;
 		ig.music.play();
-		
+
 		// Load the LevelTest as required above ('game.level.test')
 		this.loadLevel( Level1 );
 	},
-	
-	update: function() {		
+
+	update: function() {
 		// Update all entities and BackgroundMaps
 		this.parent();
 
@@ -45,14 +71,27 @@ MyGame = ig.Game.extend({
 		}
 		*/
 	},
-	
+
 	draw: function() {
 		// Draw all entities and BackgroundMaps
 		this.parent();
-		
-		this.font.draw( 'Arrow Keys, X, C', 2, 2 );
+
+		var now = new Date().getTime()
+		  , hex = _hexForTimestamp( now )
+
+		ig.game.clearColor = '#' + hex;
+
+		// Stuur de ambilight kleur maar 1 keer per seconden.
+		//if( now - _lastUpdated > 1000) {
+			// Ambilight heeft wat lag...
+			var lagHex = _hexForTimestamp( now + 1500);
+			//console.log( hex, lagHex );
+			sessionStorage.setItem('ambilightColor', lagHex);
+			_lastUpdated = now;
+		//}
 	}
 });
+
 
 
 // Start the Game with 60fps, a resolution of 240x160, scaled
