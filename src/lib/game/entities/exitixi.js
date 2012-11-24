bf42270481cc648f9bb8ef6035f02c3b95ad9f4d
Tom Lokhorst <tom@lokhorst.eu>
2012-11-24 10:29:19 +0100

    Cleanup

--- src/lib/game/entities/exitixi.js
+++ src/lib/game/entities/exitixi.js
@@ -1,83 +1,83 @@
 ig.module(
-	'game.entities.exitixi'
+  'game.entities.exitixi'
 )
 .requires(
-	'impact.entity'
+  'impact.entity'
 )
 .defines(function(){
 
 EntityExitixi = ig.Entity.extend({
-	size: {x: 94, y: 183}, // 100 - 6 magic pixels. idk...
+  size: {x: 94, y: 183}, // 100 - 6 magic pixels. idk...
 
-	type: ig.Entity.TYPE.B, // Evil enemy group
-	checkAgainst: ig.Entity.TYPE.A, // Check against friendly
-	collides: ig.Entity.COLLIDES.PASSIVE,
-	entrance: false,
+  type: ig.Entity.TYPE.B, // Evil enemy group
+  checkAgainst: ig.Entity.TYPE.A, // Check against friendly
+  collides: ig.Entity.COLLIDES.PASSIVE,
+  entrance: false,
 
-	animSheet: new ig.AnimationSheet( 'media/exitixi.png', 100, 183 ),
+  animSheet: new ig.AnimationSheet( 'media/exitixi.png', 100, 183 ),
 
 
-	init: function( x, y, settings ) {
-		this.parent( x, y, settings );
-		
-		this.addAnim( 'closed', 1, [3], true );
-		this.addAnim( 'opened', 1, [0], true );
-		this.addAnim( 'open', .15, [3, 2, 1, 0], true );
-		this.addAnim( 'close', .15, [0, 1, 2, 3], true );
+  init: function( x, y, settings ) {
+    this.parent( x, y, settings );
+    
+    this.addAnim( 'closed', 1, [3], true );
+    this.addAnim( 'opened', 1, [0], true );
+    this.addAnim( 'open', .15, [3, 2, 1, 0], true );
+    this.addAnim( 'close', .15, [0, 1, 2, 3], true );
 
-		if (this.entrance) {
-			this.currentAnim = this.anims.opened;
-		}
-		this.currentAnim.flip.x = this.entrance;
-	},
+    if (this.entrance) {
+      this.currentAnim = this.anims.opened;
+    }
+    this.currentAnim.flip.x = this.entrance;
+  },
 
-	update: function() {
-		if (ig.game.state == GameState.ENTERED && !this.entrance && this.currentAnim != this.anims.open) {
-			this.anims.open.rewind();
-			this.currentAnim = this.anims.open;
-			ig.game.state = GameState.PLAYING;
-		}
-		this.parent();
-	},
+  update: function() {
+    if (ig.game.state == GameState.ENTERED && !this.entrance && this.currentAnim != this.anims.open) {
+      this.anims.open.rewind();
+      this.currentAnim = this.anims.open;
+      ig.game.state = GameState.PLAYING;
+    }
+    this.parent();
+  },
 
-	handleMovementTrace: function( res ) {
-		this.parent( res );
-	},
+  handleMovementTrace: function( res ) {
+    this.parent( res );
+  },
 
-	closeDoor: function () {
-		if (this.currentAnim == this.anims.close || this.currentAnim == this.anims.closed) return;
-		
-		this.anims.close.rewind();
-		this.currentAnim = this.anims.close;
-		this.currentAnim.flip.x = this.entrance;
-	},
-	isClosed: function () {
-		return this.currentAnim == this.anims.close && this.currentAnim.frame == 3;
-	},
+  closeDoor: function () {
+    if (this.currentAnim == this.anims.close || this.currentAnim == this.anims.closed) return;
+    
+    this.anims.close.rewind();
+    this.currentAnim = this.anims.close;
+    this.currentAnim.flip.x = this.entrance;
+  },
+  isClosed: function () {
+    return this.currentAnim == this.anims.close && this.currentAnim.frame == 3;
+  },
 
-	check: function( other ) {
-		var isPlayer =  other instanceof EntityPlayer;
-		if (!isPlayer) return;
+  check: function( other ) {
+    var isPlayer =  other instanceof EntityPlayer;
+    if (!isPlayer) return;
 
-		if (this.entrance) {
-			other.entrance = this;
-		}
+    if (this.entrance) {
+      other.entrance = this;
+    }
 
-		if( other.state != PlayerState.PLAYING ) {
-			return;
-		}
+    if( other.state != PlayerState.PLAYING ) {
+      return;
+    }
 
-		other.state = PlayerState.EXITIXING;
-		other.exitixi = this;
+    other.state = PlayerState.EXITIXING;
+    other.exitixi = this;
 
-		// Ambilight alert!
-		console.info('Sending alert to ambilight.');
-		// try {
-		// 	var img = new Image();
-		// 	img.src = 'http://www.huelandsspoor.nl/api/effects/alert';
-		// }
-		// catch (_) {}
-	}
+    // Ambilight alert!
+    console.info('Sending alert to ambilight.');
+    // try {
+    //   var img = new Image();
+    //   img.src = 'http://www.huelandsspoor.nl/api/effects/alert';
+    // }
+    // catch (_) {}
+  }
 });
 
 });
--- src/lib/game/entities/operatorixi.js
+++ src/lib/game/entities/operatorixi.js
@@ -1,49 +1,49 @@
 ig.module(
-	'game.entities.operatorixi'
+  'game.entities.operatorixi'
 )
 .requires(
-	'impact.entity'
+  'impact.entity'
 )
 .defines(function(){
-	
+  
 EntityOperatorixi = ig.Entity.extend({
-	size: {x: 340 - 57*2, y: 227 - 17*2},
-	offset: {x: 57, y: 17},
-	
-	type: ig.Entity.TYPE.B, // Evil enemy group
-	checkAgainst: ig.Entity.TYPE.A, // Check against friendly
-	collides: ig.Entity.COLLIDES.NEVER,
-	
-	animSheet: new ig.AnimationSheet( 'media/operatorixi.png', 340, 227 ),
-	
-	
-	init: function( x, y, settings ) {
-		this.parent( x, y, settings );
+  size: {x: 340 - 57*2, y: 227 - 17*2},
+  offset: {x: 57, y: 17},
+  
+  type: ig.Entity.TYPE.B, // Evil enemy group
+  checkAgainst: ig.Entity.TYPE.A, // Check against friendly
+  collides: ig.Entity.COLLIDES.NEVER,
+  
+  animSheet: new ig.AnimationSheet( 'media/operatorixi.png', 340, 227 ),
+  
+  
+  init: function( x, y, settings ) {
+    this.parent( x, y, settings );
 
-		this.addAnim( 'idle', 1, [0] );
-		// TODO animatie versoepelen
+    this.addAnim( 'idle', 1, [0] );
+    // TODO animatie versoepelen
         this.addAnim( 'death', .3, [0], true );
-	},
-	
-	
-	update: function() {
+  },
+  
+  
+  update: function() {
         if (this.anims.death.loopCount > 0) {
             this.kill();
         }
-		this.parent();
-	},
+    this.parent();
+  },
 
     cleanup: function() {
-    	this.currentAnim = this.anims.death;
+      this.currentAnim = this.anims.death;
     },
 
-	handleMovementTrace: function( res ) {
-		this.parent( res );
-	},	
-	
-	check: function( other ) {
-		//finish
-	}
+  handleMovementTrace: function( res ) {
+    this.parent( res );
+  },  
+  
+  check: function( other ) {
+    //finish
+  }
 });
 
 });
