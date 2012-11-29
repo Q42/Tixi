13dfadfeb5f913156548a782702d6caa84346334
Tom Lokhorst <tom@lokhorst.eu>
2012-11-29 16:26:38 +0100

    Clip text from intro and outro images

--- src/lib/game/entities/intro.js
+++ src/lib/game/entities/intro.js
@@ -7,13 +7,13 @@ ig.module(
 .defines(function(){
   
 EntityIntro = ig.Entity.extend({
-  size: {x: 409, y: 249},
+  size: {x: 409, y: 200},
   
   type: ig.Entity.TYPE.B, // Evil enemy group
   checkAgainst: ig.Entity.TYPE.A, // Check against friendly
   collides: ig.Entity.COLLIDES.FIXED,
   
-  animSheet: new ig.AnimationSheet( 'media/intro.png', 409, 249 ),
+  animSheet: new ig.AnimationSheet( 'media/intro.png', 409, 200 ),
   
   init: function( x, y, settings ) {
     this.parent( x, y, settings );
